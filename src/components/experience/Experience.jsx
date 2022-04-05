309566475412a4b9980ad966be8d2b744f3c23fe
Leandro Magallanes <thesharkcommunity@outlook.com>
2022-04-05 00:28:19 -0300

    agregar experience y services

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -2,9 +2,54 @@ import React from 'react'
 
 import './experience.css'
 
+import {BsPatchCheckFill} from 'react-icons/bs'
+
 function Experience() {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className=' container experience__container'>
+        <div className='experience-item'>
+          <h3>Front-End Developer</h3>
+          <div className="experience__content">
+            
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__icon'/>
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__icon'/>
+              <h4>CSS</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__icon'/>
+              <h4>JavaScript</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__icon'/>
+              <h4>React</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__icon'/>
+              <h4>Bootstrap</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+
+          </div>
+        </div>
+
+       
+          
+      </div>
+
+
+    </section>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,128 @@
 import React from 'react'
 import './services.css'
+import {BiCheck} from 'react-icons/bi'
 
 function Services() {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className='container services__container'>
+
+        <div className='service'>
+          
+          <div className='service__head'>
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>User Research</p>
+            </li>
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>Wireframing</p>
+            </li>
+            <li>
+              <BiCheck  className="service__list-icon"/>  
+              <p>Prototyping</p>
+            </li>
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>User Testing</p>
+            </li>
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>User Experience Design</p>
+            </li>
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>User Interface Design</p>
+            </li>
+
+          </ul>
+
+        </div>
+
+        <div className='service'>
+
+          <div className='service__head'>
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>HTML5</p>
+            </li>
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>CSS3</p>
+            </li>
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>JavaScript</p>
+            </li>
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>React</p>
+            </li>
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>Node.js</p>
+            </li>
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>Express</p>
+            </li>
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>MongoDB</p>
+            </li>
+          </ul>
+        
+        </div>
+
+        <div className='service'>
+          <div className='service__head'>
+           <h3>Content Creation</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>Copywriting</p>
+            </li>
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>Content Strategy</p>
+            </li>
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>Content Creation</p>
+            </li>
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>Content Editing</p>
+            </li>
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>Content Management</p>
+            </li>
+            <li>
+              <BiCheck  className="service__list-icon"/>
+              <p>Content Marketing</p>
+            </li>
+          </ul>
+        
+        </div>
+      
+
+
+      </div>
+          
+    </section>
   )
 }
 
